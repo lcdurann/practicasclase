4318596ba378a6cf94ba356f9a037160f74ae0a0
Luis C Duran <lcduranne@unal.edu.co>
2017-08-11 09:10:44 -0700

    wazza

---
+++ hola.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(void)
+{ /* hkshkshkfsk
+fjdsifidf
+jfj*/
+  char name[150],age[3];
+  std::cout<<" Hola mundo"<<std::endl;
+  std::cout<<"Tu nombre: ";
+  std:: cin>>name;
+  std::cout<<"edad: ";
+  std:: cin>>age;
+  std::cout<<name<<std::endl;
+  std::cout<<age;
+  std::cout<<"  años"<<std::endl;
+  return 0;
+  
+}
